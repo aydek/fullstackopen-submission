4af70f4c2045ea990ba80a0dee69d1e0bcad871f
aydek <kesa0915@hotmail.com>
2023-06-01 02:36:24 +0100

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,41 +1,48 @@
 import { useState } from 'react'
+import Numbers from './components/Numbers';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '8888-2312320' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('');
-  const [newPhone, setNewPhone] = useState('')
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const submitForm = (event) => {
     event.preventDefault();
-    if(!newName.length || !newPhone.length) return
+    if(!newName.length || !newNumber.length) return
     if(persons.find(value => value.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName('');
-      setNewPhone('')
+      setNewNumber('')
       return;
     }
-    setPersons([...persons, {name: newName, phone: `${newPhone}`}]);
+    setPersons([...persons, {name: newName, number: `${newNumber}`}]);
     setNewName('');
-    setNewPhone('')
+    setNewNumber('')
   }
 
   const setPersonName = (event) => setNewName(event.target.value);
-  const setPersonPhone = (event) => setNewPhone(event.target.value);
+  const setPersonNumber = (event) => setNewNumber(event.target.value);
+  const setFiterValue = (event) => setFilter(event.target.value);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with<input value={filter} onChange={setFiterValue} /></div>
+      <h2>add a new</h2>
       <form onSubmit={submitForm}>
         <div>name: <input value={newName} onChange={setPersonName} /></div>
-        <div>number: <input value={newPhone} onChange={setPersonPhone}/></div>
+        <div>number: <input value={newNumber} onChange={setPersonNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
+      <Numbers persons={persons} filter={filter} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Numbers.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const FilteredResult = ({ persons, filter }) => {
+    const result = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    
+    return result.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+    
+}
+
+const Numbers = ({ persons, filter }) => {
+
+    return (
+        <div>
+            <h2>Numbers</h2>
+            {filter.length ? <FilteredResult persons={persons} filter={filter} /> : persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+        </div>
+    )
+}
+
+export default Numbers
