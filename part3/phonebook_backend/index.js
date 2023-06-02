a0c0b049f800ae4468742d74c86660b56befb7cc
aydek <kesa0915@hotmail.com>
2023-06-02 22:52:18 +0100

    added front end build

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use(express.static('build'))
 
 morgan.token('body', req => {
     return JSON.stringify(req.body)
