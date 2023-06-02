959449858bfccd390e0c729b2611154b2ed49bd4
aydek <kesa0915@hotmail.com>
2023-06-03 00:19:54 +0100

    Completed 3.12

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://kesa0915:${password}@cluster0.xrd9dhu.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://kesa0915:${password}@cluster0.xrd9dhu.mongodb.net/personApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -20,12 +20,22 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    name: 'Default',
-    number: 'Default',
-})
+if(!process.argv[3]) {
+    Person.find({}).then(result => {
+        console.log("phonebook:");
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        })
+        mongoose.connection.close();
+    })
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
