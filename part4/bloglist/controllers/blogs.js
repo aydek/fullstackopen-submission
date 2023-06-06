58562e6c1dc90ad67dc257d56904faaf0c70aa42
aydek <kesa0915@hotmail.com>
2023-06-06 20:25:15 +0100

    Update blogs.js

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,7 +3,6 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 const config = require('../utils/config');
-const middleware = require('../utils/middleware');
 
 blogsRouter.get('/:id', async (request, response) => {
     const result = await Blog.findById(request.params.id);
@@ -15,45 +14,57 @@ blogsRouter.get('/', async (request, response) => {
     response.json(result);
 });
 
-blogsRouter.post('/', middleware.tokenExtractor, async (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     if (!request.body.url || !request.body.title) {
         return response.status(400).end();
     }
+   
+    const user = request.user;
+    const token = request.token;
 
-    const token = jwt.verify(request.token, config.SECRET);
+    if (!token) response.status(401).json({ error: 'token invalid' });
+    if (!user) response.status(401).json({ error: 'user invalid' });
 
-    if (!token.id) {
+    const authorized = jwt.verify(token, config.SECRET);
+
+    if (!authorized) {
         return response.status(401).json({ error: 'token invalid' });
     }
 
-    const user = await User.findById(token.id);
+    const dbUser = await User.findById(user.id);
 
     const newBlog = {
         author: request.body.author,
         title: request.body.title,
         url: request.body.url,
         likes: request.body.likes,
-        user: user.id,
+        user: dbUser.id,
     };
 
     const blog = new Blog(newBlog);
 
-    user.blogs = user.blogs.concat(blog._id);
-    await user.save();
+    dbUser.blogs = dbUser.blogs.concat(blog._id);
+    await dbUser.save();
 
     const result = await blog.save();
     response.status(201).json(result);
 });
 
-blogsRouter.delete('/:id', middleware.tokenExtractor, async (request, response) => {
-    const token = jwt.verify(request.token, config.SECRET);
+blogsRouter.delete('/:id', async (request, response) => {
+    const user = request.user;
+    const token = request.token;
 
-    if (!token.id) {
+    if (!token) response.status(401).json({ error: 'token invalid' });
+    if (!user) response.status(401).json({ error: 'user invalid' });
+
+    const authorized = jwt.verify(token, config.SECRET);
+
+    if (!authorized) {
         return response.status(401).json({ error: 'token invalid' });
     }
 
     const blog = await Blog.findById(request.params.id);
-    if (blog.user.toString() === token.id) {
+    if (blog.user.toString() === user.id) {
         await Blog.findByIdAndRemove(request.params.id);
         response.status(204).end();
     } else {
