fdb9b2a7def85720420e22ebdf497d03c2bf5c2c
aydek <kesa0915@hotmail.com>
2023-06-06 16:07:45 +0100

    Update blogs.js

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,8 @@
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const config = require('../utils/config');
 
 blogsRouter.get('/:id', async (request, response) => {
     const result = await Blog.findById(request.params.id);
@@ -7,7 +10,7 @@ blogsRouter.get('/:id', async (request, response) => {
 });
 
 blogsRouter.get('/', async (request, response) => {
-    const result = await Blog.find({});
+    const result = await Blog.find({}).populate('user', { username: 1, name: 1 });
     response.json(result);
 });
 
@@ -15,7 +18,26 @@ blogsRouter.post('/', async (request, response) => {
     if (!request.body.url || !request.body.title) {
         return response.status(400).end();
     }
-    const blog = new Blog(request.body);
+
+    const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
+
+    const newBlog = {
+        author: request.body.author,
+        title: request.body.title,
+        url: request.body.url,
+        likes: request.body.likes,
+        user: user.id,
+    };
+
+    const blog = new Blog(newBlog);
+
+    user.blogs = user.blogs.concat(blog._id);
+    await user.save();
+
     const result = await blog.save();
     response.status(201).json(result);
 });
@@ -31,5 +53,13 @@ blogsRouter.put('/:id', async (request, response) => {
     const result = await Blog.findByIdAndUpdate(request.params.id, { likes }, { new: true, context: 'query' });
     response.status(200).json(result);
 });
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '');
+    }
+    return null;
+};
 
 module.exports = blogsRouter;
