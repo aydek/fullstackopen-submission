20321b6fdbad6c6b53139beb1aaad2adaa1b648d
aydek <kesa0915@hotmail.com>
2023-06-07 19:58:10 +0100

    Update login.js

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -8,7 +8,7 @@ loginRouter.post('/', async (request, response) => {
     const { username, password } = request.body;
 
     const user = await User.findOne({ username });
-    console.log(user);
+  
     const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.password);
 
     if (!passwordCorrect) {
