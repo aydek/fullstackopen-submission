a968d6fdd55eaa133e4227fe37e3f77351e3b848
aydek <kesa0915@hotmail.com>
2023-06-07 20:38:42 +0100

    Implemented login

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,17 +1,48 @@
 import { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
     const [blogs, setBlogs] = useState([]);
+    const [user, setUser] = useState(null);
+
+    const handleLogout = () => {
+        setUser(null);
+        window.localStorage.removeItem('blogsUser');
+    };
 
     useEffect(() => {
-        blogService.getAll().then((blogs) => setBlogs(blogs));
+        const getBlogs = async () => {
+            try {
+                const blogs = await blogService.getAll();
+                setBlogs(blogs);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getBlogs();
     }, []);
 
-    return (
+    useEffect(() => {
+        if (user) return;
+        const loggeduser = window.localStorage.getItem('blogsUser');
+        if (loggeduser) {
+            const user = JSON.parse(loggeduser);
+            setUser(user);
+            //noteService.setToken(user.token);
+        }
+    }, [user]);
+
+    return !user ? (
+        <LoginForm setUser={setUser} />
+    ) : (
         <div>
             <h2>blogs</h2>
+            <p>
+                {user.name} logged in <button onClick={handleLogout}>Logout</button>
+            </p>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import LoginService from '../services/login';
+
+const LoginForm = ({ setUser }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const submitLoginForm = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await LoginService.login(username, password);
+            setUser(response);
+            window.localStorage.setItem('blogsUser', JSON.stringify(response));
+        } catch (error) {
+            setErrorMessage(error.response.data.error);
+        }
+    };
+
+    useEffect(() => {
+        setErrorMessage('');
+    }, [username, password]);
+
+    return (
+        <form onSubmit={submitLoginForm}>
+            <h1>Log in to application</h1>
+            <div>
+                username:
+                <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div>
+                password:
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+            </div>
+            {errorMessage.length > 0 ? <div style={{ color: '#aa0000' }}>{errorMessage}</div> : null}
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
