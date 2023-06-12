83eedb258737d625f4c54cf9a8891f76ebbd5c79
aydek <kesa0915@hotmail.com>
2023-06-12 23:27:40 +0100

    Completed 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,42 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { increaseVote, newAnecdote } from './reducers/anecdoteReducer';
+import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-    const anecdotes = useSelector((state) => state);
-    const dispatch = useDispatch();
-
-    const vote = (id) => {
-        dispatch(increaseVote(id));
-    };
-
-    const anecdoteSubmit = (e) => {
-        e.preventDefault();
-        const content = e.target.anecdote.value;
-        e.target.anecdote.value = '';
-        dispatch(newAnecdote(content));
-    };
-
     return (
         <div>
             <h2>Anecdotes</h2>
-            {anecdotes
-                .sort((a, b) => b.votes - a.votes)
-                .map((anecdote) => (
-                    <div key={anecdote.id}>
-                        <div>{anecdote.content}</div>
-                        <div>
-                            has {anecdote.votes}
-                            <button onClick={() => vote(anecdote.id)}>vote</button>
-                        </div>
-                    </div>
-                ))}
-            <h2>create new</h2>
-            <form onSubmit={anecdoteSubmit}>
-                <div>
-                    <input name="anecdote" />
-                </div>
-                <button type="submit">create</button>
-            </form>
+            <AnecdoteList />
+            <AnecdoteForm />
         </div>
     );
 };
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { newAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch();
+
+    const anecdoteSubmit = (e) => {
+        e.preventDefault();
+        const content = e.target.anecdote.value;
+        e.target.anecdote.value = '';
+        dispatch(newAnecdote(content));
+    };
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={anecdoteSubmit}>
+                <div>
+                    <input name="anecdote" />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    );
+};
+
+export default AnecdoteForm;
