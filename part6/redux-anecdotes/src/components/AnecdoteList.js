6939a3c259a2e4ffc86fde151e6900cbcb837fe4
aydek <kesa0915@hotmail.com>
2023-06-13 02:10:35 +0100

    Update AnecdoteList.js

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increaseVote, setAnecdotes } from '../reducers/anecdoteReducer';
+import { increaseVote, initializeAnecdotes } from '../reducers/anecdoteReducer';
 import { setNotification } from '../reducers/notificationReducer';
-import anecdoteService from '../services/anecdotes';
 
 const AnecdoteList = () => {
     const anecdotes = useSelector(({ filter, anecdotes }) => {
@@ -11,13 +10,13 @@ const AnecdoteList = () => {
     const dispatch = useDispatch();
 
     const vote = (anecdote) => {
-        dispatch(increaseVote(anecdote.id));
+        dispatch(increaseVote(anecdote));
         dispatch(setNotification(`you voted '${anecdote.content}'`));
     };
 
     useEffect(() => {
-        anecdoteService.getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+        dispatch(initializeAnecdotes());
+    }, [dispatch]);
 
     return anecdotes
         .sort((a, b) => b.votes - a.votes)
