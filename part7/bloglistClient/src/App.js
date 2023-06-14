0a0fdbd56cf23973c7049751ab94cc3d51ed744f
aydek <kesa0915@hotmail.com>
2023-06-14 05:02:54 +0100

    Update App.js

--- part7/bloglistClient/src/App.js
+++ part7/bloglistClient/src/App.js
@@ -1,22 +1,19 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
-
 import { useDispatch, useSelector } from 'react-redux';
 import Notification from './components/Notification';
 import { initializeBlogs } from './reducers/blogReducer';
-import { deleteUserData, initUser } from './reducers/userReducer';
+import { initUser } from './reducers/userReducer';
 import Bloglist from './components/Bloglist';
 import Users from './components/Users';
 import User from './components/User';
+import Blog from './components/Blog';
+import Nav from './components/Nav';
 
 const App = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
-
-    const handleLogout = () => {
-        dispatch(deleteUserData());
-    };
 
     useEffect(() => {
         dispatch(initializeBlogs());
@@ -27,16 +24,15 @@ const App = () => {
         <LoginForm />
     ) : (
         <Router>
+            <Nav />
             <h2>blogs</h2>
             <Notification />
-            <p>
-                {user.name} logged in <button onClick={handleLogout}>Logout</button>
-            </p>
 
             <Routes>
                 <Route path="/" element={<Bloglist />} />
                 <Route path="/users" element={<Users />} />
                 <Route path="/users/:id" element={<User />} />
+                <Route path="/blogs/:id" element={<Blog />} />
             </Routes>
         </Router>
     );
