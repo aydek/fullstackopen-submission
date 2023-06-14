cc8e020c3815d2e3d70ca4d05cf9a110535b06f0
aydek <kesa0915@hotmail.com>
2023-06-14 19:48:46 +0100

    Update Nav.js

--- part7/bloglistClient/src/components/Nav.js
+++ part7/bloglistClient/src/components/Nav.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+
 import { deleteUserData } from '../reducers/userReducer';
+import { AppBar, Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const user = useSelector((state) => state.user);
 
     const handleLogout = () => {
         dispatch(deleteUserData());
     };
     return (
-        <div style={{ backgroundColor: 'gray', padding: 5 }}>
-            <Link style={{ padding: 5 }} to="/">
-                blogs
-            </Link>
-            <Link style={{ padding: 5 }} to="/users">
-                users
-            </Link>
-            {user.name} logged in <button onClick={handleLogout}>Logout</button>
-        </div>
+        <AppBar position="static" color="primary">
+            <Box sx={{ display: 'flex', alignItems: 'center', p: 1 }}>
+                <Typography sx={{ mr: 2, display: { xs: 'none', md: 'block' } }}>Blogs app</Typography>
+                <Button color="inherit" onClick={() => navigate('/')}>
+                    Blogs
+                </Button>
+                <Button color="inherit" onClick={() => navigate('/users')}>
+                    Users
+                </Button>
+                <Box sx={{ ml: 'auto', display: 'flex', alignItems: 'center' }}>
+                    <Typography sx={{ display: { xs: 'none', md: 'block' } }}>{user.name} logged in </Typography>
+                    <Button color="inherit" onClick={handleLogout}>
+                        Logout
+                    </Button>
+                </Box>
+            </Box>
+        </AppBar>
     );
 };
 
