85fee3db0fcec487e5d14d1b15cd8554a908060f
aydek <kesa0915@hotmail.com>
2023-06-14 20:47:34 +0100

    Update User.js

--- part7/bloglistClient/src/components/User.js
+++ part7/bloglistClient/src/components/User.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import UserService from '../services/users';
+import { Box, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+import CommentIcon from '@mui/icons-material/Comment';
 
 const User = () => {
     const id = useParams().id;
@@ -16,14 +18,31 @@ const User = () => {
         fetch();
     }, []);
 
-    return !user ? (
-        <div>User not found...</div>
-    ) : (
-        <div>
-            <h2>{user.name}</h2>
-            <h3>Added blogs</h3>
-            <ul>{user.blogs.length > 0 ? user.blogs.map((blog) => <li key={blog.id}>{blog.title}</li>) : <div>No blogs found...</div>}</ul>
-        </div>
+    return (
+        <Paper sx={{ p: 2, mt: 2 }}>
+            {!user ? (
+                <Typography>User not found...</Typography>
+            ) : (
+                <Box>
+                    <Typography variant="h4" sx={{ mb: 3 }}>
+                        {user.name}
+                    </Typography>
+                    <Typography variant="h5">Added blogs</Typography>
+                    <List>
+                        {user.blogs.length > 0 ? (
+                            user.blogs.map((blog) => (
+                                <ListItem key={blog.id}>
+                                    <CommentIcon />
+                                    <ListItemText inset={true} primary={blog.title}></ListItemText>
+                                </ListItem>
+                            ))
+                        ) : (
+                            <Typography>No blogs found...</Typography>
+                        )}
+                    </List>
+                </Box>
+            )}
+        </Paper>
     );
 };
 
