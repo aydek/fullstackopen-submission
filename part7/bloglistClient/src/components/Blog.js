9e28911675c0b4a64d8a7ba44fdd3466b5fbe75c
aydek <kesa0915@hotmail.com>
2023-06-14 20:54:01 +0100

    Update Blog.js

--- part7/bloglistClient/src/components/Blog.js
+++ part7/bloglistClient/src/components/Blog.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { addLike, removeBlog } from '../reducers/blogReducer';
 import BlogComments from './BlogComments';
+import { Box, Button, Link, Paper, Typography } from '@mui/material';
 
 const Blog = () => {
     const id = useParams().id;
@@ -26,19 +27,27 @@ const Blog = () => {
     };
 
     return (
-        <div className="blog">
-            <h2>
-                {blog.title} {blog.author}
-            </h2>
-            <a href={blog.url}>{blog.url}</a>
-            <div>
-                likes <span>{blog.likes}</span>
-                <button onClick={() => handleLike(blog)}>like</button>
-            </div>
-            <div>adde by: {blog.user && blog.user.name}</div>
-            {blog.user && blog.user.username === user.username ? <button onClick={() => handleRemove(blog)}>Delete</button> : null}
+        <Paper className="blog" sx={{ p: 2, mt: 2 }}>
+            <Typography variant="h4">
+                {blog.title} by {blog.author}
+            </Typography>
+            <Link href={blog.url}>{blog.url}</Link>
+            <Box display={'flex'} alignItems={'center'}>
+                <Typography>
+                    Likes: <strong>{blog.likes}</strong>
+                </Typography>
+                <Button onClick={() => handleLike(blog)}>like</Button>
+            </Box>
+            <Typography>
+                Added by: <strong>{blog.user && blog.user.name}</strong>
+            </Typography>
+            {blog.user && blog.user.username === user.username ? (
+                <Button variant="outlined" onClick={() => handleRemove(blog)}>
+                    Delete
+                </Button>
+            ) : null}
             <BlogComments blog={blog} />
-        </div>
+        </Paper>
     );
 };
 
