9bed666a9225b4ec7aaf2dc90ba64b409206961d
aydek <kesa0915@hotmail.com>
2023-06-14 04:09:33 +0100

    Update Bloglist.js

--- part7/bloglistClient/src/components/Bloglist.js
+++ part7/bloglistClient/src/components/Bloglist.js
@@ -1,12 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Blog from './Blog';
+import Toggable from './Toggable';
+import { addBlog } from '../reducers/blogReducer';
+import AddBlogForm from './AddBlogForm';
 
 const Bloglist = () => {
+    const blogFormRef = useRef();
+    const dispatch = useDispatch();
+
+    const handleNewBlog = (title, author, url) => {
+        blogFormRef.current.toggleVisibility();
+        dispatch(addBlog(title, author, url));
+    };
     const blogs = useSelector((state) => state.blogs);
-    return blogs
-        .slice()
-        .sort((a, b) => b.likes - a.likes)
-        .map((blog) => <Blog key={blog.id} blog={blog} />);
+    return (
+        <>
+            <Toggable label="New Blog" show={false} hideLabel="Cancel" ref={blogFormRef}>
+                <AddBlogForm handleNewBlog={handleNewBlog} />
+            </Toggable>
+            {blogs
+                .slice()
+                .sort((a, b) => b.likes - a.likes)
+                .map((blog) => (
+                    <Blog key={blog.id} blog={blog} />
+                ))}
+        </>
+    );
 };
 
 export default Bloglist;
