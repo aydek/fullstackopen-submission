7c3e4fcbd7a0c32ef48ca3a431000d08d70f5c5a
aydek <kesa0915@hotmail.com>
2023-06-14 04:21:39 +0100

    Update Users.js

--- part7/bloglistClient/src/components/Users.js
+++ part7/bloglistClient/src/components/Users.js
@@ -1,19 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import UserService from '../services/users';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
         const fetch = async () => {
-            const users = await UserService.getAll();
-            console.log(users);
+            const response = await UserService.getAll();
+            setUsers(response);
         };
-
         fetch();
     }, []);
 
     return (
         <div>
             <h2>Users</h2>
+            {users.length ? (
+                <table>
+                    <thead>
+                        <tr>
+                            <td> </td>
+                            <td>
+                                <strong>blogs created</strong>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{user.blogs.length}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <div>No users found...</div>
+            )}
         </div>
     );
 };
