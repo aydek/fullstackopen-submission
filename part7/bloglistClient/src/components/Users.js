e34cb1615c28ad2e7330f48990be096e3e8c5403
aydek <kesa0915@hotmail.com>
2023-06-14 05:03:06 +0100

    Update Users.js

--- part7/bloglistClient/src/components/Users.js
+++ part7/bloglistClient/src/components/Users.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import UserService from '../services/users';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -28,7 +29,9 @@ const Users = () => {
                     <tbody>
                         {users.map((user) => (
                             <tr key={user.id}>
-                                <td>{user.name}</td>
+                                <td>
+                                    <Link to={`/users/${user.id}`}>{user.name}</Link>
+                                </td>
                                 <td>{user.blogs.length}</td>
                             </tr>
                         ))}
