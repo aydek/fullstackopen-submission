79193a9b948a0e5611c743a0ae981fa5a5583cd3
aydek <kesa0915@hotmail.com>
2023-06-14 20:18:34 +0100

    Update Notification.js

--- part7/bloglistClient/src/components/Notification.js
+++ part7/bloglistClient/src/components/Notification.js
@@ -1,17 +1,16 @@
+import { Alert } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 const Notification = () => {
     const notification = useSelector((state) => state.notification);
 
-    const style = {
-        border: 'solid',
-        padding: 10,
-        borderWidth: 1,
-        marginBottom: 10,
-        color: notification.type === 'error' ? 'red' : 'green',
-    };
-
-    return notification.message.length > 0 && <div style={style}>{notification.message}</div>;
+    return (
+        notification.message.length > 0 && (
+            <Alert sx={{ mt: 2 }} severity={notification.type}>
+                {notification.message}
+            </Alert>
+        )
+    );
 };
 
 export default Notification;
