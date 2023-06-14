b5eba8c1639e8621feec2b447af46a5c37054bb5
aydek <kesa0915@hotmail.com>
2023-06-14 05:02:57 +0100

    Update Blog.js

--- part7/bloglistClient/src/components/Blog.js
+++ part7/bloglistClient/src/components/Blog.js
@@ -1,19 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
-import Toggable from './Toggable';
-import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
 import { addLike, removeBlog } from '../reducers/blogReducer';
 
-const Blog = ({ blog }) => {
+const Blog = () => {
+    const id = useParams().id;
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
+    const blogs = useSelector((state) => state.blogs);
 
-    const blogStyle = {
-        paddingTop: 10,
-        paddingLeft: 2,
-        border: 'solid',
-        borderWidth: 1,
-        marginBottom: 5,
-    };
+    const blog = blogs.find((blog) => blog.id === id);
+
+    if (!blog) return;
 
     const handleLike = (blog) => {
         dispatch(addLike(blog));
@@ -26,23 +23,19 @@ const Blog = ({ blog }) => {
     };
 
     return (
-        <div style={blogStyle} className="blog">
-            <span>{blog.title}</span> <span>{blog.author}</span>
-            <Toggable label="View" show={false} buttonAtTop={true} className="view-button">
-                <div>{blog.url}</div>
-                <div>
-                    likes <span>{blog.likes}</span>
-                    <button onClick={() => handleLike(blog)}>like</button>
-                </div>
-                <div>{blog.user && blog.user.username}</div>
-                {blog.user && blog.user.username === user.username ? <button onClick={() => handleRemove(blog)}>Delete</button> : null}
-            </Toggable>
+        <div className="blog">
+            <h2>
+                {blog.title} {blog.author}
+            </h2>
+            <a href={blog.url}>{blog.url}</a>
+            <div>
+                likes <span>{blog.likes}</span>
+                <button onClick={() => handleLike(blog)}>like</button>
+            </div>
+            <div>adde by: {blog.user && blog.user.name}</div>
+            {blog.user && blog.user.username === user.username ? <button onClick={() => handleRemove(blog)}>Delete</button> : null}
         </div>
     );
 };
 
 export default Blog;
-
-Blog.propTypes = {
-    blog: PropTypes.object.isRequired,
-};
