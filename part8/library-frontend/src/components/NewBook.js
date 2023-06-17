a6230c63651202ba58a0e1b78c0fae7ce92d89ca
aydek <kesa0915@hotmail.com>
2023-06-17 05:01:26 +0100

    Update NewBook.js

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
 import { useState } from 'react';
 import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../querys';
+import { Alert, Button, Paper, Stack, TextField, Typography } from '@mui/material';
 
 const NewBook = () => {
     const [title, setTitle] = useState('');
@@ -8,8 +9,14 @@ const NewBook = () => {
     const [published, setPublished] = useState('');
     const [genre, setGenre] = useState('');
     const [genres, setGenres] = useState([]);
+    const [error, setError] = useState('');
 
-    const [createBook] = useMutation(CREATE_BOOK, { refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }] });
+    const [createBook] = useMutation(CREATE_BOOK, {
+        refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+        onError: (error) => {
+            setError(error.graphQLErrors[0].message);
+        },
+    });
 
     const submit = async (event) => {
         event.preventDefault();
@@ -29,30 +36,41 @@ const NewBook = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={submit}>
-                <div>
-                    title
-                    <input value={title} onChange={({ target }) => setTitle(target.value)} />
-                </div>
-                <div>
-                    author
-                    <input value={author} onChange={({ target }) => setAuthor(target.value)} />
-                </div>
-                <div>
-                    published
-                    <input type="number" value={published} onChange={({ target }) => setPublished(target.value)} />
-                </div>
-                <div>
-                    <input value={genre} onChange={({ target }) => setGenre(target.value)} />
-                    <button onClick={addGenre} type="button">
-                        add genre
-                    </button>
-                </div>
-                <div>genres: {genres.join(' ')}</div>
-                <button type="submit">create book</button>
-            </form>
-        </div>
+        <Paper component="form" onSubmit={submit} sx={{ p: 2 }}>
+            <Stack maxWidth={200}>
+                <TextField margin="normal" label="Title" name="title" autoComplete="title" autoFocus value={title} onChange={(e) => setTitle(e.target.value)} />
+                <TextField margin="normal" label="Author" name="author" autoComplete="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+
+                <TextField
+                    margin="normal"
+                    type="number"
+                    label="Published"
+                    name="published"
+                    autoComplete="published"
+                    value={published}
+                    onChange={(e) => setPublished(e.target.value)}
+                />
+            </Stack>
+            <Stack direction="row">
+                <TextField
+                    margin="normal"
+                    variant="standard"
+                    type="text"
+                    label="Genre"
+                    name="genre"
+                    autoComplete="genre"
+                    value={genre}
+                    onChange={(e) => setGenre(e.target.value)}
+                />
+                <Button onClick={addGenre}>Add</Button>
+            </Stack>
+
+            <Typography>Genres: {genres.join(' ')}</Typography>
+            <Button variant="outlined" type="submit">
+                Create book
+            </Button>
+            {error.length > 0 && <Alert severity="error">{error}</Alert>}
+        </Paper>
     );
 };
 
