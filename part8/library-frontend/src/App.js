9787a5bdf23958899239c284899ac1b6e74907ad
aydek <kesa0915@hotmail.com>
2023-06-17 03:56:30 +0100

    Update App.js

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,20 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import Nav from './components/Nav';
+import { useEffect, useState } from 'react';
+import LoginForm from './components/LoginFrom';
+import { useApolloClient } from '@apollo/client';
 
 const App = () => {
+    const [token, setToken] = useState(null);
+    const client = useApolloClient();
+
+    const logout = () => {
+        setToken(null);
+        localStorage.clear();
+        client.resetStore();
+    };
+
+    useEffect(() => {
+        const storageToken = localStorage.getItem('library-app-token');
+        if (storageToken) setToken(storageToken);
+    }, []);
+
     return (
         <Router>
-            <div>
-                <Link to="/">Authors</Link>
-                <Link to="/books">Books</Link>
-                <Link to="/add">Add book</Link>
-            </div>
+            <Nav token={token} logout={logout} />
             <Routes>
                 <Route path="/" element={<Authors />} />
                 <Route path="/books" element={<Books />} />
                 <Route path="/add" element={<NewBook />} />
+                {!token && <Route path="/login" element={<LoginForm setToken={setToken} />} />}
             </Routes>
         </Router>
     );
