caed0712cd8e5a1cc6fa7a69d97046f0325f9980
aydek <kesa0915@hotmail.com>
2023-06-18 07:51:55 +0100

    Added genre filter

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,35 +1,63 @@
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../querys';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, Button, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Book from './Book';
 
 const Books = () => {
     const result = useQuery(ALL_BOOKS);
-    if (result.loading) {
-        return <div>loading...</div>;
-    }
-    const books = result.data.allBooks;
+    const [filter, setFilter] = useState('');
+    const [allGenres, setAllGenres] = useState([]);
 
-    return (
-        <TableContainer component={Paper} sx={{ width: '80%', p: 2 }}>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Book</TableCell>
-                        <TableCell>Author</TableCell>
-                        <TableCell>Published</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {books.map((a) => (
-                        <TableRow key={a.title}>
-                            <TableCell>{a.title}</TableCell>
-                            <TableCell>{a.author.name}</TableCell>
-                            <TableCell>{a.published}</TableCell>
+    const books = !result.loading && result.data.allBooks;
+
+    useEffect(() => {
+        if (!result.loading && books) {
+            const genres = [];
+            for (const book of books) {
+                for (const genre of book.genres) {
+                    if (!genres.includes(genre) && genre.length > 0) {
+                        genres.push(genre);
+                    }
+                }
+            }
+            setAllGenres(genres);
+        }
+    }, [books, result.loading]);
+
+    return result.loading ? (
+        <Typography>loading...</Typography>
+    ) : (
+        <Box>
+            {filter.length > 0 && (
+                <Typography>
+                    In genre: <strong>{filter}</strong>
+                </Typography>
+            )}
+            <TableContainer component={Paper} sx={{ width: '80%', p: 2 }}>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Book</TableCell>
+                            <TableCell>Author</TableCell>
+                            <TableCell>Published</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>{filter.length > 0 ? <Book books={books.filter((book) => book.genres.includes(filter))} /> : <Book books={books} />}</TableBody>
+                </Table>
+                <Stack direction="row" spacing={1} useFlexGap flexWrap="wrap" sx={{ mt: 2 }}>
+                    {allGenres.length > 1 &&
+                        allGenres.map((genre) => (
+                            <Button key={genre} variant="outlined" onClick={() => setFilter(genre)}>
+                                {genre}
+                            </Button>
+                        ))}
+                    <Button variant="outlined" color="secondary" onClick={() => setFilter('')}>
+                        All genres
+                    </Button>
+                </Stack>
+            </TableContainer>
+        </Box>
     );
 };
 
