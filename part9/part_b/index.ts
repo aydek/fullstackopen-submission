5cc6c2d8dd0bd42b05c64d71818cc7ab66363ac3
aydek <kesa0915@hotmail.com>
2023-06-19 10:23:50 +0100

    Update index.ts

--- part9/part_b/index.ts
+++ part9/part_b/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+import { isNotNumber } from './utils';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const height = req.query.height;
+    const weight = req.query.weight;
+
+    if (!height || !weight || isNotNumber(height) || isNotNumber(weight)) res.status(400).send({ error: 'malformatted parameters' });
+    else res.json({ weight, height, bmi: calculateBmi(Number(height), Number(weight)) });
 });
 
 const PORT = 3000;
