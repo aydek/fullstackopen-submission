acd83b2c9a3b65291ca3865e6b9b64145dd74751
aydek <kesa0915@hotmail.com>
2023-06-19 08:14:12 +0100

    Create bmi

---
+++ part9/BMI/bmiCalculator.ts
@@ -0,0 +1,48 @@
+const conditions = [
+    { maxBmi: 16, label: 'Underweight (Severe thinness)' },
+    { maxBmi: 17, label: 'Underweight (Moderate thinness)' },
+    { maxBmi: 18.5, label: 'Underweight (Mild thinness)' },
+    { maxBmi: 25, label: 'Normal range' },
+    { maxBmi: 30, label: 'Overweight (Pre-obese)' },
+    { maxBmi: 35, label: 'Obese (Class I)' },
+    { maxBmi: 40, label: 'Obese (Class II)' },
+    { maxBmi: Number.POSITIVE_INFINITY, label: 'Obese (Class III)' },
+];
+
+interface ArgValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]): ArgValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3]),
+        };
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+};
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / Math.pow(height / 100, 2);
+    if (bmi < 16) return 'Underweight (Severe thinness)';
+    for (const condition of conditions) {
+        if (bmi < condition.maxBmi) {
+            return condition.label;
+        }
+    }
+};
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+}
