5c535ff32108bb0f528f772ac2e12a7ddd865553
aydek <kesa0915@hotmail.com>
2023-06-20 20:54:01 +0100

    Update App.tsx

--- part9/part_d/diaries/frontend/src/App.tsx
+++ part9/part_d/diaries/frontend/src/App.tsx
@@ -1,4 +1,14 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { DiaryEntry } from './types';
+
 function App() {
+    useEffect(() => {
+        axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then((response) => {
+            console.log(response.data);
+        });
+    }, []);
+
     return (
         <>
             <div>app</div>
