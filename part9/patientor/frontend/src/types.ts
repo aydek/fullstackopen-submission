b31489c0dffd43d9ff1b27e5193970a57c1e963d
aydek <kesa0915@hotmail.com>
2023-06-22 15:19:15 +0100

    Update types.ts

--- part9/patientor/frontend/src/types.ts
+++ part9/patientor/frontend/src/types.ts
@@ -1,22 +1,67 @@
 export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
+    code: string;
+    name: string;
+    latin?: string;
 }
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+    Male = 'male',
+    Female = 'female',
+    Other = 'other',
 }
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CriticalRisk' = 3,
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface BaseEntry {
+    id: string;
+    date: string;
+    type: string;
+    specialist: string;
+    diagnosisCodes?: Diagnosis['code'][];
+    description?: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating?: HealthCheckRating;
+}
+
+type Entry = OccupationalHealthcareEntry | HospitalEntry | HealthCheckEntry;
 
 export interface Patient {
-  id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+    id: string;
+    name: string;
+    dateOfBirth: string;
+    ssn: string;
+    gender: Gender;
+    occupation: string;
+    entries: Entry[];
 }
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export type PatientFormValues = Omit<Patient, 'id' | 'entries'>;
