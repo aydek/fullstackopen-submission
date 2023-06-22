7fc784c6a50d0f00e2155bdb7098fde46666a578
aydek <kesa0915@hotmail.com>
2023-06-22 17:52:55 +0100

    Update patients.ts

--- part9/patientor/backend/routes/patients.ts
+++ part9/patientor/backend/routes/patients.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import patientsService from '../services/patientsService';
-import { NewPatientEntry } from '../types';
+import { EntryWithoutId, NewPatientEntry } from '../types';
 
 const router = express.Router();
 
@@ -21,10 +21,26 @@ router.get('/:id', (req, res) => {
         }
     }
 });
+
 router.post('/', (req, res) => {
     try {
         const { name, occupation, ssn, dateOfBirth, gender, entries } = req.body as NewPatientEntry;
         const newEntry = patientsService.addPatient({ name, occupation, ssn, dateOfBirth, gender, entries });
+        res.json(newEntry);
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong.';
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
+});
+
+router.post('/:id/entries', (req, res) => {
+    try {
+        const { id } = req.params;
+        const entry = req.body as EntryWithoutId;
+        const newEntry = patientsService.addEntry(id, entry);
         res.json(newEntry);
     } catch (error: unknown) {
         let errorMessage = 'Something went wrong.';
