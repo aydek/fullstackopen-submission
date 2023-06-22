1c9d0ece3f612862b58ae35eadc316a6979465c2
aydek <kesa0915@hotmail.com>
2023-06-22 15:07:54 +0100

    Update index.tsx

--- part9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from '../../constants';
+import { Patient } from '../../types';
+import { Box, Typography } from '@mui/material';
+import { Female, Male } from '@mui/icons-material';
 
-const index = () => {
-    return (
-        <div>
-            <div>aa</div>
-        </div>
+const PatientPage = () => {
+    const id = useParams().id;
+
+    const [patient, setPatients] = useState<Patient>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get(`${apiBaseUrl}/patients/${id}`);
+            setPatients(result.data);
+        };
+
+        void fetchData();
+    }, [id]);
+
+    return !patient ? null : (
+        <Box>
+            <Typography variant="h5" fontWeight={'bold'} sx={{ my: 2 }}>
+                {patient.name} {patient.gender === 'female' ? <Female /> : <Male />}
+            </Typography>
+            <Typography>ssn: {patient.ssn}</Typography>
+            <Typography>occupation: {patient.occupation}</Typography>
+        </Box>
     );
 };
 
-export default index;
+export default PatientPage;
